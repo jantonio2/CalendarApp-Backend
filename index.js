906031f0d818abe05a25fcd8ed4728ee889f0641
jantonio2 <jesusrochavedia@gmail.com>
2021-11-22 08:57:16 -0400

    Instalacion de express y configuración

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const app = express();
 
-console.log('Hola mundo');
+app.get('/', function(req, res) {
+  res.json({
+    ok: true
+  });
+});
+
+app.listen(4000, function() {
+  console.log(`Servidor corriendo en puerto ${4000}`);
+});
