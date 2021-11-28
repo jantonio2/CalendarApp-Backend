c005738958b33421e7f69e5b24995631c7adb14d
jantonio2 <jesusrochavedia@gmail.com>
2021-11-28 15:42:55 -0400

    Eliminando un evento

--- controllers/events.js
+++ controllers/events.js
@@ -77,11 +77,40 @@ const actualizarEvento = async(req, res = response) => {
   }
 };
 
-const eliminarEvento = (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: 'eliminarEvento'
-  });
+const eliminarEvento = async(req, res = response) => {
+
+  try {
+    const eventoId = req.params.id;
+    const evento = await Evento.findById(eventoId);
+    const uid = req.uid;
+
+    if(!evento){
+      return  res.status(404).json({
+        ok: false,
+        msg: 'No existe un evento con ese id'
+      });
+    }
+
+    if(evento.user.toString() !== uid){
+      return res.status(401).json({
+        ok: false,
+        msg: 'No tiene privilegio de eliminar este evento'
+      });
+    }
+
+    await Evento.findByIdAndDelete(eventoId);
+
+    res.json({
+      ok: true
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Pongase en contacto con el administrador'
+    });
+  }
 };
 
 module.exports = {
