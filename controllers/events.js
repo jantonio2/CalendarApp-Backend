8e5f2c65b3fd8c30522661087573b89a69c1820e
jantonio2 <jesusrochavedia@gmail.com>
2021-11-28 14:55:44 -0400

    Realizando la actualizacion de los eventos

--- controllers/events.js
+++ controllers/events.js
@@ -1,15 +1,18 @@
 const { response } = require('express');
 const Evento = require('../models/Evento');
 
-const getEventos = (req, res = response) => {
+const getEventos = async(req, res = response) => {
+
+  const eventos = await Evento.find()
+                              .populate('user', 'name'); 
+
   res.json({
     ok: true,
-    msg: 'getEventos'
+    eventos
   });
 };
 
 const crearEvento = async(req, res = response) => {
-
   
   try {
     const evento = new Evento(req.body);
@@ -32,11 +35,46 @@ const crearEvento = async(req, res = response) => {
   }
 };
 
-const actualizarEvento = (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: 'actualizarEvento'
-  });
+const actualizarEvento = async(req, res = response) => {
+
+  try {
+    const eventoId = req.params.id;
+    const evento = await Evento.findById(eventoId);
+    const uid = req.uid;
+
+    if(!evento){
+      return  res.status(404).json({
+        ok: false,
+        msg: 'No existe un evento con ese id'
+      });
+    }
+
+    if(evento.user.toString() !== uid){
+      return res.status(401).json({
+        ok: false,
+        msg: 'No tiene privilegio de editar este evento'
+      });
+    }
+
+    const nuevoEvento = {
+      ...req.body,
+      user: uid
+    };
+
+    const eventoActualizado = await Evento.findByIdAndUpdate(eventoId, nuevoEvento, { new: true });
+
+    res.json({
+      ok: true,
+      evento: eventoActualizado
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Pongase en contacto con el administrador'
+    });
+  }
 };
 
 const eliminarEvento = (req, res = response) => {
--- routes/events.js
+++ routes/events.js
@@ -26,7 +26,15 @@ router.post(
   crearEvento
 );
 
-router.put('/:id', actualizarEvento);
+router.put(
+  '/:id',
+  [
+    check('title', 'El titulo es obligatorio').not().isEmpty(),
+    check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+    check('end', 'Fecha de finalización es obligatoria').custom(isDate),
+    validarCampos
+  ],
+  actualizarEvento);
 
 router.delete('/:id', eliminarEvento);
 
